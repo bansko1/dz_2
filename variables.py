e493e60c69ccb4bc7798269d32251b9989571414
bansko1 <alexvp@bk.ru>
2023-05-17 23:54:47 +0300

    add files

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Описание стула
+name = 'Венский стул'
+print(type(name))
+legs = 4
+print(type(legs))
+weight = 3.5
+print(type(weight))
+is_antique = True
+print(type(is_antique))
+material = 'oak'
+print(type(material))
+owner = None
+print(type(owner))
